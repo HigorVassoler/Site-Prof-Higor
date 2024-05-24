1d7dcf9863b1811e132b36b8e724b738b61b5276
HigorVassoler <107952648+HigorVassoler@users.noreply.github.com>
2024-05-24 09:21:30 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -36,9 +36,20 @@ function calculaTempo(tempoObjetivo) {
     return dias + "dias " + horas + "horas " + minutos + "minutos " + segundos + "segundos";
 }
 
-function atualizaCronometro(){
+function atualizaCronometro() {
     document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
     document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
     document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
     document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
+
+    for (let i = 0; i < contadores.length; i++) {
+        // contadores[i].textContent = calculaTempo(tempos[i]);
+    }
 }
+
+function comecaCronometro() {
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro();
